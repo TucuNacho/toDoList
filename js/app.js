34f59a5bc7129fdf8cee1c946eabd000839c5303
Nacho <barrojojoaquin2@gmail.com>
2025-06-08 00:14:53 -0300

    estructura para crear y agregar una tabla para guardar paginas a desarrollar

--- js/app.js
+++ js/app.js
@@ -1,13 +1,74 @@
+import Paginas from "./classeTarea.js";
+
 //funciones
 const abrirModal = () => {
-  //aqui abro la ventana modal
-  modalTarea.show();
-  //cambie la variabl para que cree peliculas
+  const modalElement = document.getElementById("modalWeb"); // o el ID que tengas
+  const modalWeb = new bootstrap.Modal(modalElement);
+  modalWeb.show();
 };
-console.log("Hola mundo desde app.js");
+const crearWeb = () => {
+  const lista = new Paginas(
+    inputWeb.value,
+    inputFechaInicio.value,
+    inputFechaTermino.value,
+    inputEstado.value,
+    inputPrioridad.value
+  );
+  toDoList.push(lista);
+  // guardar en localStorage
+  guardarLocalStorage();
+  // dibujar la fila en la tabla
+  dibujarFila(lista, toDoList.length);
+  resetForm();
+};
 
+const resetForm = () => {
+  formWeb.reset();
+};
+
+const guardarLocalStorage = () => {
+  localStorage.setItem("toDoListKey", JSON.stringify(toDoList));
+};
+
+const cargarWeb = () => {
+  if (toDoList > 0) {
+    toDoList.map((web, indice) => dibujarFila(web, indice + 1));
+  }
+};
+
+const dibujarFila = (web, indice) => {
+  tablaWeb.innerHTML += `<tr>
+              <th scope="row">${indice}</th>
+              <td>${web.pagina}</td>
+              <td>${web.fechaInicio}</td>
+              <td>${web.fechaTerminar}</td>
+              <td>${web.estado}</td>
+              <td>${web.prioridad}</td>
+              <td>
+                <button class="btn btn-warning"onclick="prepararContacto('${web.id}')" >Editar</button>
+                <button class="btn btn-danger" onclick="eliminarContacto('${web.id}')" >Borrar</button>
+                <button class="btn btn-info" onclick="verContacto('${web.id}')">Ver</button>
+              </td>
+            </tr>`;
+};
+// Variables
 const BtnAgregar = document.getElementById("btnAgregar");
+const formWeb = document.querySelector("form");
+const inputWeb = document.getElementById("web");
+const inputFechaInicio = document.getElementById("fechaI");
+const inputFechaTermino = document.getElementById("fechaF");
+const inputEstado = document.getElementById("estado");
+const inputPrioridad = document.getElementById("prioridad");
+const tablaWeb = document.querySelector("tbody");
+const toDoList = [];
 
+// Eventos
 BtnAgregar.addEventListener("click", abrirModal);
+formWeb.addEventListener("submit", (e) => {
+  e.preventDefault();
+  crearWeb();
+  console.log("Formulario enviado");
+});
 //aqui abro la ventana modal
 //cambie la variabl para que cree peliculas;
+cargarWeb();
---
+++ js/classeTarea.js
@@ -0,0 +1,65 @@
+export default class Paginas {
+  #id;
+  #pagina;
+  #fechaInicio;
+  #fechaTerminar;
+  #estado;
+  #prioridad;
+  constructor(pagina, fechaInicio, fechaTerminar, estado, prioridad) {
+    this.#id = crypto.randomUUID();
+    this.#pagina = pagina;
+    this.#fechaInicio = fechaInicio;
+    this.#fechaTerminar = fechaTerminar;
+    this.#estado = estado;
+    this.#prioridad = prioridad;
+  }
+
+  get pagina() {
+    return this.#pagina;
+  }
+
+  get fechaInicio() {
+    return this.#fechaInicio;
+  }
+
+  get fechaTerminar() {
+    return this.#fechaTerminar;
+  }
+
+  get estado() {
+    return this.#estado;
+  }
+  get prioridad() {
+    return this.#prioridad;
+  }
+
+  set pagina(nuevaPagina) {
+    this.#pagina = nuevaPagina;
+  }
+
+  set fechaInicio(nuevaFechaInicio) {
+    this.#fechaInicio = nuevaFechaInicio;
+  }
+
+  set fechaTerminar(nuevaFechaTerminar) {
+    this.#fechaTerminar = nuevaFechaTerminar;
+  }
+
+  set estado(nuevoEstado) {
+    this.#estado = nuevoEstado;
+  }
+  set prioridad(nuevaPrioridad) {
+    this.#prioridad = nuevaPrioridad;
+  }
+
+  toJSON() {
+    return {
+      id: this.#id,
+      tarea: this.#pagina,
+      fechaInicio: this.#fechaInicio,
+      fechaTerminar: this.#fechaTerminar,
+      estado: this.#estado,
+      prioridad: this.#prioridad,
+    };
+  }
+}
