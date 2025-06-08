6648d47518006c7e92157a0eebbd988ba62da02c
Nacho <barrojojoaquin2@gmail.com>
2025-06-07 22:16:23 -0300

    estructura principal para una ToDoList

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//funciones
+const abrirModal = () => {
+  //aqui abro la ventana modal
+  modalTarea.show();
+  //cambie la variabl para que cree peliculas
+};
+console.log("Hola mundo desde app.js");
+
+const BtnAgregar = document.getElementById("btnAgregar");
+
+BtnAgregar.addEventListener("click", abrirModal);
+//aqui abro la ventana modal
+//cambie la variabl para que cree peliculas;
